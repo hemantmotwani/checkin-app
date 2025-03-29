9e1da7f786783a7d5a0f0092a03d4e98c4c7f5ac
hemantmotwani <hemantmotwani@gmail.com>
2025-03-28 23:13:28 -0500

    made changes to fix the error in vercel

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
   // No need for manual 'define' if using VITE_* variables
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        dashboard: resolve(__dirname, 'dashboard.html')
+      }
+    }
+  }
 });
